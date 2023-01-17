e14fceff92d0312798f9ff4ec46039a50882e774
David Witt <dhw5111@psu.edu>
2023-01-17 11:45:54 -0500

    Lab2 Commit

--- src/lab2/cities_n_routes.py
+++ src/lab2/cities_n_routes.py
@@ -4,6 +4,9 @@ Lab 2: Cities and Routes
 In the final project, you will need a bunch of cities spread across a map. Here you 
 will generate a bunch of cities and all possible routes between them.
 '''
+import random
+import itertools
+
 
 def get_randomly_spread_cities(size, n_cities):
     """
@@ -11,33 +14,55 @@ def get_randomly_spread_cities(size, n_cities):
     and returns a list of cities with their x and y coordinates. The cities are randomly spread
     across the map.
     
-    :param size: the size of the map as a tuple of 2 integers
+    :param size: the size of the map as a tuple of 2 integers (100,100) 
     :param n_cities: The number of cities to generate
     :return: A list of cities with random x and y coordinates.
+
+    import random
+    random.randint()
+    To create lists Cities = []
+    Then For Loop
+        Cities.Append()
+    
+    return cities
     """
     # Consider the condition where x size and y size are different
-    pass
 
-def get_routes(cities):
+    cities = []
+    cords = ()
+    for i in range(n_cities):
+        cords = [random.randint(0,size[0]),random.randint(0,size[1])]
+        cities.append(cords)
+    return cities
+    pass
+def get_routes(city_names):
     """
     It takes a list of cities and returns a list of all possible routes between those cities
     
-    :param cities: a list of cities, each of which is a tuple of coordinates
+    :param city_names: a list of cities names
     :return: A list of tuples representing all possible links between cities, 
             each item in the list (a link) represents a route between two cities.
-    """
-    pass
 
+    import itertools
+
+    for a_combinations in itertools.combinations(a, 2):
+        print(a_combinations)
+    """
+    cities_toreturn = []
+    for city_combinations in itertools.combinations(city_names,2):
+        cities_toreturn.append(city_combinations)
+    return cities_toreturn
+    pass
 
 # TODO: Fix variable names
 if __name__ == '__main__':
     city_names = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
     '''print the cities and routes'''
     cities = get_randomly_spread_cities((100, 100), 10)
-    routes = get_routes(cities)
+    routes = get_routes(city_names)
     print('Cities:')
     for i, city in enumerate(cities):
         print(f'{city_names[i]}: {city}')
     print('Routes:')
     for i, route in enumerate(routes):
-        print(f'{i}: {city_names[route[0]]} to {city_names[route[1]]}')
+        print(f'{i}: {route[0]} to {route[1]}')
