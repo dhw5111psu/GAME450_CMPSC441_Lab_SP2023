bd64ecd9b94be9ffaf40a1a9b468ea6036e37e52
David Witt <dhw5111@psu.edu>
2023-03-14 11:29:10 -0400

    Lab10

--- src/lab10/lab10.py
+++ src/lab10/lab10.py
@@ -1,16 +1,17 @@
 """ Lab 10: Save people
 You can save people from heart disease by training a model to predict whether a person has heart disease or not.
-The dataset is available at src/lab8/heart.csv
+The dataset is available at src/lab10/heart.csv
 Train a model to predict whether a person has heart disease or not and test its performance.
 You can usually improve the model by normalizing the input data. Try that and see if it improves the performance. 
 """
 from sklearn.metrics import confusion_matrix
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn import preprocessing #Import For Data Normalization
 import pandas as pd
 import numpy as np
 
-data = pd.read_csv("src/lab8/heart.csv")
+data = pd.read_csv("src/lab10/heart.csv")
 
 # Transform the categorical variables into dummy variables.
 print(data.head())
@@ -26,12 +27,17 @@ x_train, x_test, y_train, y_test = train_test_split(
 
 """ Train a sklearn model here. """
 
-sklearn_model = None
+sklearn_model = KNeighborsClassifier(n_neighbors=11)
+sklearn_model.fit(x_train,y_train)
 
 # Accuracy
 print("Accuracy of model: {}\n".format(sklearn_model.score(x_test, y_test)))
 
 
 """ Improve the model by normalizing the input data. """
+scalar = preprocessing.MinMaxScaler()
+x_train = scalar.fit_transform(x_train)
+x_test = scalar.fit_transform(x_test)
+sklearn_model.fit(x_train,y_train)
 
 print("Accuracy of improved model: {}\n".format(sklearn_model.score(x_test, y_test)))
